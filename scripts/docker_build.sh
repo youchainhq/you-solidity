21634f4a43ec65ac728e415f21506a94ab6c5514
liangdaji <liangdaji@youchain.cc>
2020-02-03 18:28:50 +0800

    build: change docker_build

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Scratch image
-docker build -t ethereum/solc:build -f scripts/Dockerfile .
-tmp_container=$(docker create ethereum/solc:build sh)
+docker build -t youchain/solc:build -f scripts/Dockerfile .
+tmp_container=$(docker create youchain/solc:build sh)
 mkdir -p upload
 docker cp ${tmp_container}:/usr/bin/solc upload/solc-static-linux
 
 # Alpine image
-docker build -t ethereum/solc:build-alpine -f scripts/Dockerfile_alpine .
+docker build -t youchain/solc:build-alpine -f scripts/Dockerfile_alpine .
